fec31e5346737de363a66e8184f506b920e7d615
GNaD <cpt.meoz@gmail.com>
2023-01-04 17:49:50 +0700

    correct comment

--- x/feeabs/keeper/exchange_rate.go
+++ x/feeabs/keeper/exchange_rate.go
@@ -6,14 +6,14 @@ import (
 	"github.com/notional-labs/feeabstraction/v1/x/feeabs/types"
 )
 
-// SetIbcTokenExchangeRate set ibc-token/native-token pair exchange rate
+// SetIbcTokenExchangeRate set native-token/ibc-token pair exchange rate
 func (k Keeper) SetIbcTokenFeeExchangeRate(ctx sdk.Context, denom string, exchangeRate sdk.Dec) {
 	store := ctx.KVStore(k.storeKey)
 	bz := k.cdc.MustMarshal(&sdk.DecProto{Dec: exchangeRate})
 	store.Set(types.GetIbcTokenFeeExchangeRateKey(denom), bz)
 }
 
-// GetIbcTokenExchangeRate get ibc-token/native-token pair exchange rate
+// GetIbcTokenExchangeRate get native-token/ibc-token pair exchange rate
 func (k Keeper) GetIbcTokenFeeExchangeRate(ctx sdk.Context, denom string) (sdk.Dec, error) {
 	store := ctx.KVStore(k.storeKey)
 	bz := store.Get(types.GetIbcTokenFeeExchangeRateKey(denom))
