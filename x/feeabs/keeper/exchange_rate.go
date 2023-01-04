8655fdc3072e9c208d9797c5f6247d68be7366f6
GNaD <cpt.meoz@gmail.com>
2023-01-04 16:18:42 +0700

    change key name

--- x/feeabs/keeper/exchange_rate.go
+++ x/feeabs/keeper/exchange_rate.go
@@ -10,13 +10,13 @@ import (
 func (k Keeper) SetIbcTokenFeeExchangeRate(ctx sdk.Context, denom string, exchangeRate sdk.Dec) {
 	store := ctx.KVStore(k.storeKey)
 	bz := k.cdc.MustMarshal(&sdk.DecProto{Dec: exchangeRate})
-	store.Set(types.GetIbcTokenExchangeRateKey(denom), bz)
+	store.Set(types.GetIbcTokenFeeExchangeRateKey(denom), bz)
 }
 
 // GetIbcTokenExchangeRate get ibc-token/native-token pair exchange rate
 func (k Keeper) GetIbcTokenFeeExchangeRate(ctx sdk.Context, denom string) (sdk.Dec, error) {
 	store := ctx.KVStore(k.storeKey)
-	bz := store.Get(types.GetIbcTokenExchangeRateKey(denom))
+	bz := store.Get(types.GetIbcTokenFeeExchangeRateKey(denom))
 	if bz == nil {
 		return sdk.ZeroDec(), sdkerrors.Wrapf(types.ErrInvalidExchangeRate, "Token %s not have exchange rate data", denom)
 	}
