22f26d1af7b6fe2474fa4c81b51c99aa9badcc7d
catShaark <gundamaster5@gmail.com>
2023-01-04 18:36:53 +0700

    update key.go

--- x/feeabs/types/keys.go
+++ x/feeabs/types/keys.go
@@ -15,21 +15,10 @@ const (
 )
 
 var (
-	NativeTokenExchangeRateKeyPrefix = []byte{0x01} // Key prefix for native token exchange rate
-	IbcTokenExchangeRateKeyPrefix    = []byte{0x02} // Key prefix for ibc token exchange rate
+	OsmosisFeeRate = []byte{0x01} // Key for fee rate of ibc osmosis token
 )
 
 // GetNativeTokenExchangeRateKey return the native-token exchange rate key prefix
-func GetNativeTokenExchangeRateKey() (key []byte) {
-	key = append(key, NativeTokenExchangeRateKeyPrefix...)
-	key = append(key, []byte(KeySeparator)...)
-	return key
-}
-
-// GetIbcTokenFeeExchangeRateKey return the ibc-token exchange rate key prefix
-func GetIbcTokenFeeExchangeRateKey(denom string) (key []byte) {
-	key = append(key, IbcTokenExchangeRateKeyPrefix...)
-	key = append(key, []byte(denom)...)
-	key = append(key, []byte(KeySeparator)...)
-	return key
+func GetOsmosisFeeRateKey() (key []byte) {
+	return OsmosisFeeRate
 }
