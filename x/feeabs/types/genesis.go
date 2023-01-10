09ba848e093940f2ad1e2f53028127ff50eb415f
vuong177 <nguyenvuong1122000@gmail.com>
2023-01-10 13:47:40 +0700

    portid

--- x/feeabs/types/genesis.go
+++ x/feeabs/types/genesis.go
@@ -16,6 +16,7 @@ func DefaultGenesis() *GenesisState {
 			AccumulatedOsmosisFeeSwapPeriod: DefaultSwapPeriod,
 		},
 		Epochs: []EpochInfo{NewGenesisEpochInfo("swap", DefaultQueryPeriod)},
+		PortId: IBCPortID,
 	}
 }
 
